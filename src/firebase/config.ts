a5e04e1faaa8b9d0912514d683edf11732c7afa7
OneStop Tech Solutions <onestoptechsolutions2@gmail.com>
2025-10-22 18:40:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7420129095-aca94",
+  "appId": "1:437681522903:web:5cdefdd4aee93db62a8892",
+  "apiKey": "AIzaSyD_IAvFVagwyTJ-wjb3XM1Z6YFPAyWRJTw",
+  "authDomain": "studio-7420129095-aca94.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "437681522903"
+};
